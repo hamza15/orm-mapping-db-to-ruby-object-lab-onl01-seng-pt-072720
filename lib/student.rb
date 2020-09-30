82e72eafdd2594dc7a7cf461205a191ec42b74e7
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-30 06:02:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,7 +90,7 @@ class Student
   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = 10
+      WHERE grade BETWEEN 10
       LIMIT 1
     SQL
     
