c08c35ab3c22f5745d177a30a5134c1ef94f2bba
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-30 05:57:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,14 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+      LIMIT 1
+    SQL
     
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
