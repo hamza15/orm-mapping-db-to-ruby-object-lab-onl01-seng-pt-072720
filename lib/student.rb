df79de1d4c46058a8d231dd64a222132b1c552ff
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-30 06:11:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,11 +111,10 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = 10
-      LIMIT 1
+      WHERE grade = ?
     SQL
     
     DB[:conn].execute(sql).map do |row|
