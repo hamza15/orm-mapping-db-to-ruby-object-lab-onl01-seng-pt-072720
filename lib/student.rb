fa2458d393ec1cf1e4ed91ccc65b61f1ceaaf2c7
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-30 06:08:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,7 +100,15 @@ class Student
   end
   
   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
     
+    DB[:conn].execute(sql, number).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
 end
